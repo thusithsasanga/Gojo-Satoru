faf5f3671be70d681a5201bb92812d99a53361c0
thusithsasanga <107309282+thusithsasanga@users.noreply.github.com>
2022-07-01 09:41:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['+94741621237']
+global.premium = ['+94741621237']
+global.ownernomer = '+94741621237'
+global.ownername = '𝙼𝚁 |𝚃 𝙷 𝚄 𝚂 𝙸_🙇‍♂️'
+global.botname = 'king bot'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
